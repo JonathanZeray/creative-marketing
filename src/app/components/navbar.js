142a01fbea6439096f99a7d93bcbae3dba2dd05c
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-07-07 14:47:05 +0200

    Removed logo.

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,8 +1,8 @@
 export default function Navbar() {
     return (
         <>
-        <div className="flex justify-start pl-4 items-center h-20 w-full mb-2">
-            <img className="relative w-32 h-12 my-2 mx-2 sm:mx-8" src="/images/lastCC_noBG.png" alt="" />
+        <div className="flex justify-center pl-4 items-center h-20 w-full mb-2">
+            {/* <img className="relative w-32 h-12 my-2 mx-2 sm:mx-8" src="/images/lastCC_noBG.png" alt="" /> */}
             <p className="text-white justify-center text-center">Some things that caught my eye.</p>
         </div>
         </>
